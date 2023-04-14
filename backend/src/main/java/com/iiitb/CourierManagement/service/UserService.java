95ef7dbde2d0882014235cba56198318a028411d
vardhan2000 <vardhan.av2000@gmail.com>
2023-04-14 14:48:29 +0530

    minor change

--- backend/src/main/java/com/iiitb/CourierManagement/service/UserService.java
+++ backend/src/main/java/com/iiitb/CourierManagement/service/UserService.java
@@ -49,15 +49,15 @@ public class UserService {
         adminUser.setRole(adminRoles);
         userDao.save(adminUser);
 
-        User user = new User();
-        user.setUserName("raj123");
-        user.setUserPassword(getEncodedPassword("raj@123"));
-        user.setUserFirstName("raj");
-        user.setUserLastName("sharma");
-        Set<Role> userRoles = new HashSet<>();
-        userRoles.add(userRole);
-        user.setRole(userRoles);
-        userDao.save(user);
+//        User user = new User();
+//        user.setUserName("raj123");
+//        user.setUserPassword(getEncodedPassword("raj@123"));
+//        user.setUserFirstName("raj");
+//        user.setUserLastName("sharma");
+//        Set<Role> userRoles = new HashSet<>();
+//        userRoles.add(userRole);
+//        user.setRole(userRoles);
+//        userDao.save(user);
     }
 
     public String getEncodedPassword(String password) {
