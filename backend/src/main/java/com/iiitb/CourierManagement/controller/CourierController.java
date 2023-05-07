02b371cc555e5f9534fea10f4554a2fa9f181fe4
vardhan2000 <vardhan.av2000@gmail.com>
2023-05-08 01:55:02 +0530

    added log statements for CourierController

--- backend/src/main/java/com/iiitb/CourierManagement/controller/CourierController.java
+++ backend/src/main/java/com/iiitb/CourierManagement/controller/CourierController.java
@@ -2,6 +2,7 @@ package com.iiitb.CourierManagement.controller;
 
 import com.iiitb.CourierManagement.entity.Courier;
 import com.iiitb.CourierManagement.service.CourierService;
+import com.iiitb.CourierManagement.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -10,73 +11,121 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @CrossOrigin
 public class CourierController {
 
-//    Logger logger = LoggerFactory.getLogger(CourierController.class);
+    Logger logger = LoggerFactory.getLogger(CourierController.class);
 
-    private static Logger logger;
-
-    @PostConstruct
-    public void initLogger() {
-        logger = LogManager.getLogger(CourierController.class);
-    }
+//    private static Logger logger = LogManager.getLogger(CourierController.class);
 
     @Autowired
     private CourierService courierService;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
     @PostMapping({"/addCourier"})
     @PreAuthorize("hasRole('Admin')")
-    public Courier addCourier(@RequestBody Courier courier) {
-        logger.info("FATAL ERROR");
-        return courierService.addCourier(courier);
+    public Courier addCourier(@RequestBody Courier courier, HttpServletRequest request) {
+        String jwtToken = request.getHeader("Authorization").substring(7);
+        String uname = jwtUtil.getUsernameFromToken(jwtToken);
+        logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] addCourier"+ " [CALLER] "+uname+ " [STATUS] ACCEPT");
+        Courier cour = courierService.addCourier(courier);
+        if(cour != null) {
+            logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] addCourier"+ " [CALLER] "+uname+ " [STATUS] SUCCESS");
+        }
+        else {
+            logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] addCourier"+ " [CALLER] "+uname+ " [STATUS] FAILURE");
+        }
+        return cour;
     }
 
     @PostMapping({"/updateCourier"})
     @PreAuthorize("hasRole('Admin')")
-    public Courier updateCourier(@RequestBody Courier courier) {
-        return courierService.updateCourier(courier);
+    public Courier updateCourier(@RequestBody Courier courier, HttpServletRequest request) {
+        String jwtToken = request.getHeader("Authorization").substring(7);
+        String uname = jwtUtil.getUsernameFromToken(jwtToken);
+        logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] updateCourier"+ " [CALLER] "+uname+ " [STATUS] ACCEPT");
+        Courier cour = courierService.updateCourier(courier);
+        if(cour != null) {
+            logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] updateCourier"+ " [CALLER] "+uname+ " [STATUS] SUCCESS");
+        }
+        else {
+            logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] updateCourier"+ " [CALLER] "+uname+ " [STATUS] FAILURE");
+        }
+        return cour;
     }
 
     @PostMapping({"/deleteCourier"})
     @PreAuthorize("hasRole('Admin')")
     public void deleteCourier(@RequestBody Courier courier, HttpServletRequest request) {
-        String header = request.getHeader("Authorization");
-        System.out.println(header);
+        String jwtToken = request.getHeader("Authorization").substring(7);
+        String uname = jwtUtil.getUsernameFromToken(jwtToken);
+        logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] deleteCourier"+ " [CALLER] "+uname+ " [STATUS] ACCEPT");
+
         courierService.deleteCourier(courier);
+
+        logger.info("[TYPE] incoming"+" [METHOD] POST"+ " [API_NAME] deleteCourier"+ " [CALLER] "+uname+ " [STATUS] SUCCESS");
     }
 
     @GetMapping({"/getMyCouriers"})
     @PreAuthorize("hasRole('User')")
     public Set<Courier> getMyCouriers(HttpServletRequest request) {
         String jwtToken = request.getHeader("Authorization").substring(7);
-        return courierService.getMyCouriers(jwtToken);
+        String uname = jwtUtil.getUsernameFromToken(jwtToken);
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getMyCouriers"+ " [CALLER] "+uname+ " [STATUS] ACCEPT");
+
+        Set<Courier> couriers = courierService.getMyCouriers(jwtToken);
+
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getMyCouriers"+ " [CALLER] "+uname+ " [STATUS] SUCCESS");
+        return couriers;
     }
 
     @GetMapping({"/getAllCouriers"})
     @PreAuthorize("hasRole('User') or hasRole('Admin')")
     public Set<Courier> getAllCouriers(HttpServletRequest request) {
-        return courierService.getAllCouriers();
+        String jwtToken = request.getHeader("Authorization").substring(7);
+        String uname = jwtUtil.getUsernameFromToken(jwtToken);
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getAllCouriers"+ " [CALLER] "+uname+ " [STATUS] ACCEPT");
+
+        Set<Courier> couriers = courierService.getAllCouriers();
+
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getAllCouriers"+ " [CALLER] "+uname+ " [STATUS] SUCCESS");
+        return couriers;
     }
 
     @GetMapping({"/getCouriersByRollNo"})
     @PreAuthorize("hasRole('Admin')")
-    public Set<Courier> getCouriersByRollNo(@RequestParam String ownerRollNo) {
-        System.out.println(courierService.getCouriersByRollNo(ownerRollNo));
-        return courierService.getCouriersByRollNo(ownerRollNo);
+    public Set<Courier> getCouriersByRollNo(@RequestParam String ownerRollNo, HttpServletRequest request) {
+        String jwtToken = request.getHeader("Authorization").substring(7);
+        String uname = jwtUtil.getUsernameFromToken(jwtToken);
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getCouriersByRollNo"+ " [CALLER] "+uname+ " [STATUS] ACCEPT");
+
+        Set<Courier> couriers = courierService.getCouriersByRollNo(ownerRollNo);
+
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getCouriersByRollNo"+ " [CALLER] "+uname+ " [STATUS] SUCCESS");
+        return couriers;
     }
 
     @GetMapping({"/getCouriersByName"})
     @PreAuthorize("hasRole('Admin')")
-    public Set<Courier> getCouriersByName(@RequestParam String owner) {
-        return courierService.getCouriersByName(owner);
+    public Set<Courier> getCouriersByName(@RequestParam String owner, HttpServletRequest request) {
+        String jwtToken = request.getHeader("Authorization").substring(7);
+        String uname = jwtUtil.getUsernameFromToken(jwtToken);
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getCouriersByName"+ " [CALLER] "+uname+ " [STATUS] ACCEPT");
+
+        Set<Courier> couriers = courierService.getCouriersByName(owner);
+
+        logger.info("[TYPE] incoming"+" [METHOD] GET"+ " [API_NAME] getCouriersByName"+ " [CALLER] "+uname+ " [STATUS] SUCCESS");
+        return couriers;
     }
 
 }
